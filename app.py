3ef7c430a1d5e49bb99d8ef4ce58fc5586353550
Elton Tay <eltontay1997@gmail.com>
2021-02-20 09:53:27 +0800

    added the navbar and searchbar and account page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -8,22 +8,19 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/library', methods=['POST'])
+@app.route('/library', methods=['GET', 'POST'])
 def library():
-    if request.method == 'POST':
-        username = request.form['user']
-        password = request.form['pass']
-        return render_template('library.html')
-    else:
-        pass
+    return render_template('library.html')
 
 
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        return render_template('signup.html')
-    else:
-        pass
+    return render_template('signup.html')
+
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    return render_template('account.html')
 
 
 if __name__ == '__main__':
